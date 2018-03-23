89607d72305b1400c249f0f7a01c3d322edbeb26
Jul Jen <juliajen.prog@gmail.com>
2018-03-23 18:12:10 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,8 +13,9 @@ class InteractiveRecord
 
 
   def save
-    row = DB[:conn].execute(
-    "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    row = DB[:conn].execute (
+    "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+    VALUES (#{values_for_insert})"
     )
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
