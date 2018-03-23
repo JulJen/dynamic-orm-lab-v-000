6fa6c465ee9041d9772e6c4d5043f10713a9cee6
Jul Jen <juliajen.prog@gmail.com>
2018-03-23 17:57:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,6 +11,7 @@ class InteractiveRecord
     end
   end
 
+
   def save
     sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
 
@@ -18,9 +19,11 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
+
 
   def self.column_names
     DB[:conn].results_as_hash = true
@@ -35,21 +38,26 @@ class InteractiveRecord
     column_names.compact
   end
 
+
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
+
   def self.find_by
-  end 
+  end
+
 
   def table_name_for_insert
     self.class.table_name
   end
 
+
   def col_names_for_insert
      self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
 
   def values_for_insert
     values = []
